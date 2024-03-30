59ab739f8d8e060fbd647a2d67b828d5863cc173
Kristijan Kocev <kristijankocev1234@gmail.com>
2024-03-30 14:57:34 +0100

    hello controller test

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -20,6 +20,7 @@ public class HelloControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
+	@SuppressWarnings("null")
 	@Test
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
